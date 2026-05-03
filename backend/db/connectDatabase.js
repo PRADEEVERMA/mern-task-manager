f554d26ff5237e43ef2700cc2367fdaffb8606d8
Pradeep Verma <pradeepverma.s2003@gmail.com>
2026-05-03 21:15:39 +0530

    fix cors

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -1,13 +1,15 @@
-import mongoose from "mongoose";
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const uri = process.env.MONGODB_URI;
+
+    if (!uri) {
+      throw new Error("MONGODB_URI is missing in env");
+    }
+
+    await mongoose.connect(uri);
     console.log("connected to database");
   } catch (error) {
-    console.error(`"Error ${error.message}`);
+    console.error("DB ERROR:", error.message);
     process.exit(1);
   }
 };
-
-export default connectDB;
